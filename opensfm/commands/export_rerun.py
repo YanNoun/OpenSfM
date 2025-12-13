43998264e7489072fb2edc17477bbad4005a173e
Yann N. <yannoun@gmail.com>
2025-12-13 19:14:09 +0100

    First stage of rerun export_rerun

---
+++ opensfm/commands/export_rerun.py
@@ -0,0 +1,33 @@
+# pyre-strict
+import argparse
+
+from opensfm.actions import export_rerun
+from opensfm.dataset import DataSet
+
+from . import command
+
+
+class Command(command.CommandBase):
+    name = "export_rerun"
+    help = "Export reconstruction to Rerun format for 3D visualization"
+
+    def run_impl(self, dataset: DataSet, args: argparse.Namespace) -> None:
+        export_rerun.run_dataset(
+            dataset,
+            output=args.output,
+            reconstruction_index=args.reconstruction_index,
+        )
+
+    def add_arguments_impl(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "--output",
+            "-o",
+            type=str,
+            help="Output .rrd file path (default: dataset_path/rerun.rrd)",
+        )
+        parser.add_argument(
+            "--reconstruction-index",
+            type=int,
+            default=0,
+            help="Index of reconstruction to export (default: 0)",
+        )
